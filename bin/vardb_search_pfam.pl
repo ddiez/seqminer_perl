7094d14a0d7f00d613a7c2d950be66dd63ff2daa
Diego Diez <diego10ruiz@gmail.com>
2008-04-10 08:13:05 +0000

    - added verbose output to stderr. - search output is dump into pfam direcory.

--- bin/vardb_search_pfam.pl
+++ bin/vardb_search_pfam.pl
@@ -5,11 +5,6 @@ use warnings;
 
 use varDB::Config;
 use varDB::SearchParam;
-#use varDB::SeqIO;
-#use varDB::Genome;
-#use varDB::Position;
-#use varDB::SearchResult;
-#use Sets;
 
 my $MODEL_DIR = "$HMMDB/$PFAM_VERSION/";
 
@@ -20,8 +15,13 @@ while (my $info = $param->next_param) {
 	$info->debug;
 	
 	my $base = $info->family."-".$info->organism_dir;
+	my $outdir = $param->dir($info, "pfam");
 	
 	$param->chdir($info, 'analysis');
-	system "hmmpfam $MODEL_DIR/Pfam_ls_b $base-protein.fa > $base-protein-pfam_ls.log";
-	system "hmmpfam $MODEL_DIR/Pfam_fs_b $base-protein.fa > $base-protein-pfam_fs.log";
+	print STDERR "* searching in Pfam_ls ... ";
+	system "hmmpfam $MODEL_DIR/Pfam_ls_b $base-protein.fa > $outdir/$base-protein-pfam_ls.log";
+	print STDERR "OK\n";
+	print STDERR "* searcing in Pfam_fs ... ";
+	system "hmmpfam $MODEL_DIR/Pfam_fs_b $base-protein.fa > $outdir/$base-protein-pfam_fs.log";
+	print STDERR "OK\n";
 }
