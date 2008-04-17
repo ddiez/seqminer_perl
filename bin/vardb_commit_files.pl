f0cc4c359c93d8120d39935ce37de65e9a38fbc8
Diego Diez <diego10ruiz@gmail.com>
2008-04-17 10:44:29 +0000

    - minor updates to vardb_commit_files.pl

--- bin/vardb_commit_files.pl
+++ bin/vardb_commit_files.pl
@@ -7,10 +7,12 @@ use varDB::Config;
 use varDB::SearchParam;
 
 my $param = new varDB::SearchParam;
+$param->debug;
 while (my $info = $param->next_param) {
+	$info->debug;
 	$param->chdir($info, 'nelson');
 	my $file = $info->family."-".$info->organism_dir."-nelson.txt";
-	print STDERR "commiting $file\n";
+	print STDERR "* commiting $file\n";
 	my $res = system "cp", $file, $VARDB_COMMIT_DIR.$info->super_family;
-	die "some error occured commiting files: $!" if $res == -1;
+	die "ERROR [commit]: some error occured commiting files: $!" if $res == -1;
 }
